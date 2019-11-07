662ac2f3a55a8b4b43924b1dce74079459bacf12
zxk0nly1 <1272667023@qq.com>
2019-11-07 20:12:44 +0800

    使用EXCEPTION异常处理图像处理错误

--- GD/12.php
+++ GD/12.php
@@ -1,4 +1,9 @@
 <?php
 include 'Water.php';
+try{
 $water=new Water;
 $water->make('1.jpeg','a.jpg');
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
--- GD/Water.php
+++ GD/Water.php
@@ -2,8 +2,14 @@
 class Water{
     public function make(string $image,string $filename =null)
     {
+        $this->checkImage($image);
         $res=$this->resource($image);
         return $this->showAction($image)($res,$filename??$image);
+    }
+    protected function checkImage($image){
+        if(!is_file($image)||getimagesize($image)===false){
+           throw new Exception ('file is not image');
+        };
     }
 
     protected function showAction(stirng $image){
