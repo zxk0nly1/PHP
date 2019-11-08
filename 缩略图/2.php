3508875fba7aaa49f5c5335fbcfb11833be802fc
zxk0nly1 <1272667023@qq.com>
2019-11-08 13:11:00 +0800

    通过EXCEPTION异常处理保证程序健壮性

--- 缩略图/2.php
+++ 缩略图/2.php
@@ -1,4 +1,9 @@
 <?php
 include 'Thumb.php';
 $thumb=new Thumb;
+try{
 $thumb->make('1.jpeg',200,200);
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
--- 缩略图/Thumb.php
+++ 缩略图/Thumb.php
@@ -6,11 +6,18 @@ class Thumb{
     }
     protected function resource(string $image)
     {
+        $this->check($image);
         $info=getimagesize($image);
         //print_r($info);
         $functions=[1=>'imagecreatefromgif',2=>'imagecreatefromjpeg',3=>'imagecreatefrompng'];
         $call=$functions[$info[2]];
         // echo $call;
         return $call($image);
+    }
+    protected function check(string $image){
+        if(!is_file($image)||getimagesize($image)===false)
+        {
+            throw new Exception("file not exists or it's not image ");
+        }
     }
 }
