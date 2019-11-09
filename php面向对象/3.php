b04c75e3471b799760ff1a0d85767c516da2fe0d
zxk0nly1 <1272667023@qq.com>
2019-11-09 17:33:09 +0800

    8.禁止重写策略

--- php面向对象/3.php
+++ php面向对象/3.php
@@ -2,7 +2,8 @@
 class Notify{
     protected $color='red';
     protected $credit=10;
-    public function message(){
+    //final关键字对父类的方法保护，禁止重写策略
+    public final function message(){
         return '<span style="color:'.$this->color.'">发送通知消息,奖励'.$this->credit().'个积分</span>';
         
     }
@@ -12,6 +13,9 @@ class Notify{
 }
 class User extends Notify
 {
+    public function toUserMessage(){
+        return "欢迎新用户进入网站";
+    }
     protected $credit=20;
     public function register(){
         return $this->message();
@@ -26,6 +30,8 @@ class Comment extends Notify
         return $this->message();
     }
 }
+echo (new User)->toUserMessage();
+echo "<hr/>";
 echo (new User)->register();
 echo "<hr/>";
 echo (new Comment)->send();
