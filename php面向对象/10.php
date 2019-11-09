318709ece3faa1b92b59e4a85ddd5966089ee005
zxk0nly1 <1272667023@qq.com>
2019-11-10 00:07:04 +0800

    19.见识__get和__set使用实例

---
+++ php面向对象/10.php
@@ -0,0 +1,47 @@
+<?php
+abstract class Query{
+    abstract protected function record(array $data);
+    public function select(){
+        $this->record(['name'=>'后盾人','age'=>21,'tel'=>'19999999999']);
+    }
+}
+class Model extends Query{
+    protected $field=[];
+    public function all(){
+        $this->select();
+      return $this->field;
+    }
+    protected function record(array $data){
+        $this->field=$data;
+    }
+    protected function __tel(){
+        return substr($this->field['tel'],0,8).'***';
+    }
+    public function __get($name)
+    {
+        if(method_exists($this,'__'.$name)){
+            return call_user_func_array([$this,'__'.$name],[]);
+        }
+        if(isset($this->field[$name])){
+            return $this->field[$name];
+        }
+        throw new Exception('参数错误');
+    }
+    public function __set($name, $value)
+    {
+        if(isset($this->field[$name])){
+             $this->field[$name]=$value;
+        }
+        else{
+            throw new Exception('参数错误');
+        }
+    }
+}
+try{
+$user=new Model;
+$user->all();
+$user->tel='12345678900';
+echo $user->tel;
+}catch(Exception $e){
+    echo $e->getMessage();
+}
