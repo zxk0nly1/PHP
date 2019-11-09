b57d25bf7e9939067fd65c5d92cdda86592155a6
zxk0nly1 <1272667023@qq.com>
2019-11-09 22:35:37 +0800

    9.抽象类与抽象方法解释

--- php面向对象/3.php
+++ php面向对象/3.php
@@ -1,11 +1,12 @@
 <?php
-class Notify{
+abstract class Notify{
     protected $color='red';
     protected $credit=10;
+    //抽象类没有实现体
+    abstract public function content();
     //final关键字对父类的方法保护，禁止重写策略
     public final function message(){
-        return '<span style="color:'.$this->color.'">发送通知消息,奖励'.$this->credit().'个积分</span>';
-        
+        return '<span style="color:'.$this->color.'">' .$this->content().',奖励'.$this->credit().'个积分</span>';
     }
     public function credit(){
         return $this->credit;
@@ -13,25 +14,23 @@ class Notify{
 }
 class User extends Notify
 {
-    public function toUserMessage(){
-        return "欢迎新用户进入网站";
-    }
     protected $credit=20;
     public function register(){
         return $this->message();
     }
-    // public function credit(){
-    //     return 5;
-    // }
+    public function content(){
+        return '感谢注册';
+    }
 }
 class Comment extends Notify
 {
     public function send(){
         return $this->message();
     }
+    public function content(){
+        return '感谢你的评论';
+    }
 }
-echo (new User)->toUserMessage();
-echo "<hr/>";
 echo (new User)->register();
 echo "<hr/>";
 echo (new Comment)->send();
