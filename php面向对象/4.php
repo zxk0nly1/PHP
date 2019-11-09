d604de7c2f2c08b46f92fdf4ac658779c778dee8
zxk0nly1 <1272667023@qq.com>
2019-11-09 22:51:46 +0800

    10.接口解释与解释

---
+++ php面向对象/4.php
@@ -0,0 +1,25 @@
+<?php
+interface InterFaceCache{
+    public function set($name,$value);
+    public function get($name);
+}
+class Mysql implements InterFaceCache{
+    public function set($name,$value){}
+    public function get($name){}
+}
+class Redis implements InterFaceCache{
+    public function set($name,$value){}
+    public function get($name){}
+}
+class Cache{
+    public static function instance(string $driver){
+        switch(strtolower($driver)){
+            case 'mysql':
+                return new Mysql;
+            case 'redis':
+                return new Redis;
+        }
+    }
+}
+$cache=Cache::instance('redis');
+var_dump($cache->get('name'));
