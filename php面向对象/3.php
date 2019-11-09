cabb183d98a512532b2c8b30679e5677d41199b0
zxk0nly1 <1272667023@qq.com>
2019-11-09 17:27:53 +0800

    7.父类方法和属性重写实例操作

--- php面向对象/3.php
+++ php面向对象/3.php
@@ -1,15 +1,24 @@
 <?php
 class Notify{
     protected $color='red';
+    protected $credit=10;
     public function message(){
-        return '<span style="color:'.$this->color.'">发送通知消息</span>';
+        return '<span style="color:'.$this->color.'">发送通知消息,奖励'.$this->credit().'个积分</span>';
+        
+    }
+    public function credit(){
+        return $this->credit;
     }
 }
 class User extends Notify
 {
+    protected $credit=20;
     public function register(){
         return $this->message();
     }
+    // public function credit(){
+    //     return 5;
+    // }
 }
 class Comment extends Notify
 {
