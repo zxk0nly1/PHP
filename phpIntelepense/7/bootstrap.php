5d6981b748cdf9be266ede31feddd982a90d997d
zxk0nly1 <1272667023@qq.com>
2019-11-10 13:23:43 +0800

    使用面向对象方式实现自动加载

--- phpIntelepense/7/bootstrap.php
+++ phpIntelepense/7/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
-spl_autoload_register(function ($class){
-    //echo $class;
-    $file=str_replace('\\','/',$class).'.php';
-    require $file;
-});
+namespace App;
+
+class Bootstrap {
+    public static  function boot(){
+        spl_autoload_register([new self,'autoload']);
+    }
+    public function autoload(string $class){
+        $file=str_replace('\\','/',$class).'.php';
+        require $file;
+    }
+}
+Bootstrap::boot();
